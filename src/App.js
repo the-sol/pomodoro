ff2c6413fba057cad3b5303139348f819785afc7
BahaaHassan <bah3ahass3n@gmail.com>
2021-05-28 16:14:20 +0300

    Feat: add Header
    Add header with (login, setting and image logo).

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import StartButton from './component/StartButton'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './component/Header'
 
 const App = () => {
   return (
     <div>
-      <StartButton />
+      <Header />
     </div>
   );
 }
--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Image from 'react-bootstrap/Image'
+import styles from './Header.module.css'
+import PomoLogo from '../../Logo/PomoLogo.png'
+import SettingLogo from '../../Logo/SettingLogo.png'
+import LoginLogo from '../../Logo/LoginLogo.png'
 
 const Header = () => {
     return (
         <div>
-            
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Image className={styles.Pomologo} src={PomoLogo} rounded />
+                <Navbar.Brand href="#home">TheSol-pomodoro</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Image className={styles.Setlogo} src={SettingLogo} rounded />
+                        <Nav.Link href="#setting">Setting</Nav.Link>
+                        <Image className={styles.Loglogo} src={LoginLogo} rounded />
+                        <Nav.Link href="#login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     )
 }
