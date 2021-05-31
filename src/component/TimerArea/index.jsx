1c02c3d1ecdf1ef6fb5f6a048e835cd0e056e490
Amna-Qassim <amnakassim6@gmail.com>
2021-05-31 22:18:30 +0300

    Feat: add a card with 2 buttons.
    Card containe text box displaying the time with (start) and (stop)
    buttons.

--- src/component/TimerArea/index.jsx
+++ src/component/TimerArea/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import styles from './TimerArea.module.css';
 
 const TimerArea = () => (
   <div>
-    none
+    <Card className={styles.card}>
+      <Card.Body className={styles.card__body}>
+        <Card.Text className={styles.card__text}>
+          00:25:00
+        </Card.Text>
+        <div className={styles.card__buttons}>
+          <Button variant="dark" className={styles.card__start_btn} size="lg">Start &#128525;</Button>
+          <Button variant="danger" className={styles.card__stop_btn} size="lg">Stop &#128564;</Button>
+        </div>
+      </Card.Body>
+    </Card>
   </div>
 );
 
