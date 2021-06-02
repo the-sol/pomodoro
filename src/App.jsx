6008f13524c11a6ddf025bf07bea45f20b083715
Amna-Qassim <amnakassim6@gmail.com>
2021-06-02 18:11:04 +0300

    Fix: use container from react-bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import TimerArea from './component/TimerArea';
 import styles from './App.module.css';
 import Footer from './component/Footer';
 
 const App = () => (
-  <div className={styles.container}>
-    <div>
-      <TimerArea />
-    </div>
-    <Footer />
-  </div>
+  <Container fluid className={styles.container}>
+    <Row className="text-center">
+      <Col><TimerArea /></Col>
+    </Row>
+    <Row>
+      <Col><Footer /></Col>
+    </Row>
+  </Container>
 );
 
 export default App;
