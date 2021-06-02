93f2c5d39ec9b07688caa9f5d01b0e2a6a719173
BahaaHassan <bah3ahass3n@gmail.com>
2021-06-02 18:45:17 +0300

    Refactor: adjust some ESLint problems and classNames.

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,27 +1,26 @@
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Image from 'react-bootstrap/Image'
-import styles from './Header.module.css'
-import PomoLogo from '../../Logo/PomoLogo.png'
-import SettingLogo from '../../Logo/SettingLogo.png'
-import LoginLogo from '../../Logo/LoginLogo.png'
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import styles from './Header.module.css';
+import pomoLogo from '../../Logo/PomoLogo.png';
+import settingLogo from '../../Logo/SettingLogo.png';
+import loginLogo from '../../Logo/LoginLogo.png';
 
-const Header = () => {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Image className={styles.pomo_logo} src={PomoLogo} rounded />
-      <Navbar.Brand href="#home">TheSol-pomodoro</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Image className={styles.setting_logo} src={SettingLogo} rounded />
-          <Nav.Link href="#setting">Setting</Nav.Link>
-          <Image className={styles.login_logo} src={LoginLogo} rounded />
-          <Nav.Link href="#login">Login</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
-}
+const Header = () => (
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Image className={styles['pomo-logo']} src={pomoLogo} rounded />
+    <Navbar.Brand href="#home">TheSol-pomodoro</Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse>
+      <Nav className="ml-auto">
+        <Image className={styles['setting-logo']} src={settingLogo} rounded />
+        <Nav.Link href="#setting">Setting</Nav.Link>
+        <Image className={styles['login-logo']} src={loginLogo} rounded />
+        <Nav.Link href="#login">Login</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
-export default Header
+export default Header;
