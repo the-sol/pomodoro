db6fd52e34aca43a58445cbd2e2ece1bed0259a8
Amna-Qassim <amnakassim6@gmail.com>
2021-06-16 01:33:33 +0300

    Fix: fix all issuse in code
    I delete these files because it is empty:
    PomodoroTimer/index.jsx, PomodoroTimer/Pomodoro.module.css,
    ShortBreakTimer/index.jsx, ShortBreakTimer/ShortBreak.module.css
    
    Make new component (settingsModal) to separate it from Header component
    
    Reduse the repetition in code for this file (SettingsForm) by using map function
    
    Move (button setting) from Modal component to Header component
    
    Change name of file from (SettingForm) to (SettingsForm)

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,52 +1,29 @@
 import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
-import SettingForm from '../SettingForm';
+import SettingsModal from '../SettingsModal';
 import styles from './Header.module.css';
 import pomoLogo from '../../Logo/PomoLogo.png';
-import settingLogo from '../../Logo/SettingLogo.png';
-import loginLogo from '../../Logo/LoginLogo.png';
 
-function SettingModal() {
+function Header() {
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const toggleShow = () => setShow(!show);
 
   return (
-    <>
-      <Button variant="outline-light" className="mb-3" onClick={handleShow}>
-        <i>setting</i>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Image className={styles['pomo-logo']} src={pomoLogo} rounded />
+      <Navbar.Brand href="#home">TheSol-pomodoro</Navbar.Brand>
+      <Button variant="outline-light" onClick={toggleShow}>
+        Settings
       </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Setting</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <SettingForm />
-        </Modal.Body>
-      </Modal>
-    </>
+      <Navbar.Collapse>
+        <Nav className="ml-auto">
+          <SettingsModal toggleShow={toggleShow} show={show} />
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
-const Header = () => (
-  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Image className={styles['pomo-logo']} src={pomoLogo} rounded />
-    <Navbar.Brand href="#home">TheSol-pomodoro</Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse>
-      <Nav className="ml-auto">
-        <Image className={styles['setting-logo']} src={settingLogo} rounded />
-        <Nav.Link href="#setting">
-          <SettingModal />
-        </Nav.Link>
-        <Image className={`${styles['login-logo']}`} src={loginLogo} rounded />
-        <Nav.Link href="#login" className="text-white">Login</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
-
 export default Header;
