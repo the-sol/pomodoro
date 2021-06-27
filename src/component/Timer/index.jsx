fde3636d02f3668fa76785838b495072a349c09a
Abubakr Osamah <abubakrusamah@gmai.com>
2021-06-28 01:27:12 +0300

    Feat: add new features
    add reset button this button is work to reset each period to it's
    defualt status.
    and the next button work to skip the current period to the next period.

--- src/component/Timer/index.jsx
+++ src/component/Timer/index.jsx
@@ -14,7 +14,7 @@ const Timer = () => {
   const [isRunning, setIsRunning] = useState(false);
   const [currentPeriod, setCurrentPeriod] = useState(INITIAL_PERIOD);
   const tickTimeoutId = useRef(0);
-  const counter = useRef(0);
+  const [counter, setCounter] = useState(0);
   useEffect(() => {
     if (!isRunning) {
       return;
@@ -23,15 +23,14 @@ const Timer = () => {
     const reset = () => {
       timeOverSoundAudio.play();
       setIsRunning(false);
-      counter.current += 1;
-      const nextPeriod = determineNextPeriod(currentPeriod, counter.current);
-      if (counter.current === 7) {
-        counter.current = 0;
+      setCounter(counter + 1);
+      const nextPeriod = determineNextPeriod(currentPeriod, counter);
+      if (counter === 6) {
+        setCounter(-1);
       }
       setCurrentPeriod(nextPeriod);
       setTime([nextPeriod.mins, nextPeriod.secs]);
     };
-
     const tick = () => decrementOneSec(mins, secs, setTime, reset);
     tickTimeoutId.current = setTimeout(tick, 1000);
   });
@@ -45,6 +44,25 @@ const Timer = () => {
     clearTimeout(tickTimeoutId.current);
   };
 
+  const handleResetClick = () => {
+    setIsRunning(false);
+    clearTimeout(tickTimeoutId.current);
+    setTime([currentPeriod.mins, currentPeriod.secs]);
+  };
+
+  const handleNextClick = () => {
+    const nextPeriod = determineNextPeriod(currentPeriod, counter);
+    clearTimeout(tickTimeoutId.current);
+    setTime([nextPeriod.mins, nextPeriod.secs]);
+    setCurrentPeriod(nextPeriod);
+    setIsRunning(false);
+    setCounter(counter + 1);
+    timeOverSoundAudio.play();
+    if (counter === 6) {
+      setCounter(-1);
+    }
+  };
+
   return (
     <>
       <h1>
@@ -55,6 +73,8 @@ const Timer = () => {
       <div className="d-flex justify-content-around">
         <Button variant="dark" size="lg" type="button" onClick={handleStartClick}>Start &#128525;</Button>
         <Button variant="info" size="lg" type="button" onClick={handleStopClick}>Stop &#128564;</Button>
+        <Button variant="info" size="lg" type="button" onClick={handleResetClick}>Reset &#9194;</Button>
+        <Button variant="dark" size="lg" type="button" onClick={handleNextClick}>Next &#9193;</Button>
       </div>
     </>
   );
