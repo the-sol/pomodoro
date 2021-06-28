b9da32226480e96b9dcfdda098cafbabb6b66b10
BahaaHassan <bah3ahass3n@gmail.com>
2021-06-28 23:47:44 +0300

    Feat: add text that shows each period
    add text that shows which period we are currently in.

--- src/component/Timer/index.jsx
+++ src/component/Timer/index.jsx
@@ -13,6 +13,7 @@ const Timer = () => {
   const [[mins, secs], setTime] = useState([INITIAL_PERIOD.mins, INITIAL_PERIOD.secs]);
   const [isRunning, setIsRunning] = useState(false);
   const [currentPeriod, setCurrentPeriod] = useState(INITIAL_PERIOD);
+  const [periodText, setPeriodText] = useState(INITIAL_PERIOD.id);
   const tickTimeoutId = useRef(0);
   const counter = useRef(0);
   useEffect(() => {
@@ -25,10 +26,11 @@ const Timer = () => {
       setIsRunning(false);
       counter.current += 1;
       const nextPeriod = determineNextPeriod(currentPeriod, counter.current);
-      if (counter.current === 7) {
+      if (counter.current === 8) {
         counter.current = 0;
       }
       setCurrentPeriod(nextPeriod);
+      setPeriodText(nextPeriod.id);
       setTime([nextPeriod.mins, nextPeriod.secs]);
     };
 
@@ -54,6 +56,7 @@ const Timer = () => {
       </h1>
       <div className="d-flex justify-content-around">
         <Button variant="dark" size="lg" type="button" onClick={handleStartClick}>Start &#128525;</Button>
+        <h3>{periodText}</h3>
         <Button variant="info" size="lg" type="button" onClick={handleStopClick}>Stop &#128564;</Button>
       </div>
     </>
