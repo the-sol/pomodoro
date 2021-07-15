7a23dd923e62de89467d393d79906df45e84ce1d
Mujtaba Al-Tameemi <mujtaba.altameemi@gmail.com>
2021-07-15 13:10:03 +0300

    Refactor: move firebase config to env vars
    Follow guide as explained here:
    https://create-react-app.dev/docs/adding-custom-environment-variables/

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyC90q2dSY4tlg2BXG-H1IVQCtdjMjVwXe4',
-  authDomain: 'sol-pomo.firebaseapp.com',
-  projectId: 'sol-pomo',
-  storageBucket: 'sol-pomo.appspot.com',
-  messagingSenderId: '439767660233',
-  appId: '1:439767660233:web:b32d2ca986b4beb1217f18',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
